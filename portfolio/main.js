1d87fe0ff814707b54bbd324d5623eba892c507d
Shivanshu Sharma <148370931+Shivanshu1405@users.noreply.github.com>
2023-11-01 22:34:24 +0530

    Delete portfolio/main.js

--- portfolio/main.js
+++
@@ -1,7 +0,0 @@
-let typed = new typed(".tex",{
-    Strings : ["Web Developer" , "Sophmore at TIET" , "SME" ,"Youtuber" ,"Executive Tech Member at LEAD" ,"Ex Discipline Secretary at TIET"],
-    typeSpeed : 100,
-    backSpeed: 100,
-    backDelay: 1000,
-    loop:true
-})

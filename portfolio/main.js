2364bcb30aa5a162a98bdab99a3173bec1b56ada
Shivanshu Sharma <148370931+Shivanshu1405@users.noreply.github.com>
2023-11-01 16:02:47 +0530

    Add files via upload

---
+++ portfolio/main.js
@@ -0,0 +1,7 @@
+let typed = new typed(".tex",{
+    Strings : ["Web Developer" , "Sophmore at TIET" , "SME" ,"Youtuber" ,"Executive Tech Member at LEAD" ,"Ex Discipline Secretary at TIET"],
+    typeSpeed : 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop:true
+})
